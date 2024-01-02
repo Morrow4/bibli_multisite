8c02be79ca8535e7c02e242a9458c46db48caae1
morrow <clement.laguiseray@gmail.com>
2024-01-02 17:06:01 +0100

    ajout de la fermeture de la connexion dans le main

--- src/main.c
+++ src/main.c
@@ -4,9 +4,11 @@
 #include <unistd.h>
 
 int main() {
+    // Ouvrir la connexion à la base de données
     connect_database.c;
 
-    
+    // Fermer la connexion à la base de données lorsque vous avez fini de l'utiliser
+    mysql_close(conn);
 
     return 0;
 }
