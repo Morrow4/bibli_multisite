e086cb6a8097a96339f9b6d099323fac090a0f9d
Mallaury Soutif <malosurfer02@gmail.com>
2024-01-04 11:01:41 +0100

    changement de switch case en int

--- src/main.c
+++ src/main.c
@@ -4,26 +4,31 @@
 #include <unistd.h>
 
 int main() {
+    uid_t user_uid = getuid();
+
     // Ouvrir la connexion à la base de données
-    connect_database.c;
+    connect_database();
+
     //Recupération du groupe (BD ou systeme au choix)
+    int user_group = get_user_type(user_uid);
+
     //Renvoyer vers les fonctions par utilisateur
-    switch(user_groupe)
+    switch(user_group) //pour faire un switch il faut apparemment une variable de type int, donc j'ai modifié tout ça en conséquent
     {
-        case admingeneral:
-            choix_admin_general.c;
+        case 1: //admingeneral
+            choix_admin_general();
             break;
 
-        case adminsite:
-            choix_admin_site.c;
+        case 2: //adminsite
+            choix_admin_site();
             break;
 
-        case inscrit:
-            choix_inscrit.c;
+        case 3: //inscrit
+            choix_inscrit();
             break;
 
-        case invite:
-            choix_invite.c;
+        case 4: //invite
+            choix_invite();
             break;
 
         default:
