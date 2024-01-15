124fafcd76cb16e361eb2cd8e5f176fa3ca2a63d
malos <malosurfer02@gmail.com>
2024-01-15 11:21:24 +0100

    ajout des fonctions de maj et de suppression d'un livre dans la bdd

--- src/fonctions_principales/Ajout_livre.c
+++ src/fonctions_principales/Ajout_livre.c
@@ -42,3 +42,56 @@ void ajout_livre(MYSQL *conn)
 
     printf("Livre ajouté avec succès !\n");
 }
+
+void mise_a_jour_livre(MYSQL *conn, char *ISBN)
+{
+    // Saisie des nouvelles informations du livre
+    char titre[255];
+    char auteur[100];
+    char edition[100];
+    char genre[100];
+
+    printf("Veuillez saisir les nouvelles informations du livre :\n");
+
+    printf("Nouveau Titre : ");
+    scanf("%254s", titre);
+
+    printf("Nouvel Auteur : ");
+    scanf("%99s", auteur);
+
+    printf("Nouvelle Edition : ");
+    scanf("%99s", edition);
+
+    printf("Nouveau Genre : ");
+    scanf("%99s", genre);
+
+    // Préparer la requête SQL pour la mise à jour du livre
+    char query[1024];
+    sprintf(query, "UPDATE Livre SET Titre='%s', Auteur='%s', Edition='%s', Genre='%s' WHERE ISBN='%s'",
+            titre, auteur, edition, genre, ISBN);
+
+    // Exécuter la requête SQL
+    if (mysql_query(conn, query))
+    {
+        fprintf(stderr, "Erreur lors de la mise à jour du livre : %s\n", mysql_error(conn));
+        return;
+    }
+
+    printf("Livre mis à jour avec succès !\n");
+}
+
+void suppression_livre(MYSQL *conn, char *ISBN)
+{
+    // Préparer la requête SQL pour la suppression du livre
+    char query[1024];
+    sprintf(query, "DELETE FROM Livre WHERE ISBN='%s'", ISBN);
+
+    // Exécuter la requête SQL
+    if (mysql_query(conn, query))
+    {
+        fprintf(stderr, "Erreur lors de la suppression du livre : %s\n", mysql_error(conn));
+        return;
+    }
+
+    printf("Livre supprimé avec succès !\n");
+}
--- src/fonctions_principales/livre.h
+++ src/fonctions_principales/livre.h
@@ -1,9 +1,13 @@
 #ifndef LIVRE_H
 #define LIVRE_H
 
+#include <mysql/mysql.h>
+
 #include <stdio.h>
 #include <stdlib.h>
 
 void ajout_livre(MYSQL *conn);
+void mise_a_jour_livre(MYSQL *conn, char *ISBN);
+void suppression_livre(MYSQL *conn, char *ISBN);
 
 #endif
