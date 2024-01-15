d6fb3072c13c2f07a185b5d20b9fe7fc5e437dbd
malos <malosurfer02@gmail.com>
2024-01-15 01:10:40 +0100

    ajout du fichier utilitaire.h

--- src/MariaDB/connect.c
+++ src/MariaDB/connect.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <mysql/mysql.h>
+#include "..\fonctions_utilitaires\utilitaire.h"
 
 void connect_database()
 {
@@ -19,24 +20,23 @@ void connect_database()
    // Mot de passe de l'utilisateur
    char db_user_password[255]; // Utilisation d'un tableau de caractère pour stocker le mot de passe
    pritnf("Veuillez entrer le mot de passe de l'utilisateur :\n ")
-   scanf("%254s", db_user_password); // Utilisation de "%s" pour les chaînes de caractères
+       scanf("%254s", db_user_password); // Utilisation de "%s" pour les chaînes de caractères
 
    // Connexion à la base de données
    if (!mysql_real_connect(
-         conn,                  // Connection
-         "mariadb.10.1.144.49", // Hôte
-         db_user,               // Nom d'utilisateur
-         db_user_password,      // Mot de passe utilisateur
-         "bibliotech",          // Base de donnée par défaut
-         3306,                  // Numéro du port
-         NULL,                  // Chemin vers le fichier socket
-         0                      // Options supplémentaires
-      ))
+           conn,                  // Connection
+           "mariadb.10.1.144.49", // Hôte
+           db_user,               // Nom d'utilisateur
+           db_user_password,      // Mot de passe utilisateur
+           "bibliotech",          // Base de donnée par défaut
+           3306,                  // Numéro du port
+           NULL,                  // Chemin vers le fichier socket
+           0                      // Options supplémentaires
+           ))
    {
       // Retourne l'échec de la connexion et ferme le gestionnaire
       fprintf(stderr, "Erreur de connection au serveur: %s\n", mysql_error(conn));
       mysql_close(conn);
       exit(1);
    }
-
 }
