9b84080f7cc231fbfa0b01674ffb5280f9392196
malos <malosurfer02@gmail.com>
2024-01-20 23:00:58 +0100

    suppression des utilitaires pour mettre toutes les fonctions dans utilitaire.c

--- src/fonctions_utilitaires/qui.h
+++
@@ -1,6 +0,0 @@
-#ifndef PROTOTYPE_QUI_H
-#define PROTOTYPE_QUI_H
-
-void qui(char **username);
-
-#endif // PROTOTYPE_QUI_H
--- src/fonctions_utilitaires/qui_et_quand.h
+++
@@ -1,8 +0,0 @@
-#ifndef PROTOTYPE_QUI_ET_QUAND_H
-#define PROTOTYPE_QUI_ET_QUAND_H
-
-#include "qui.h"
-
-void qui_et_quand(char **username, char **time_str);
-
-#endif // PROTOTYPE_QUI_ET_QUAND_H
