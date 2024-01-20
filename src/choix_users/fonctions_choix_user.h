7316aa626e11c76f06ec96a499402825012d53e2
malos <malosurfer02@gmail.com>
2024-01-20 23:16:43 +0100

    update header choix user

---
+++ src/choix_users/fonctions_choix_user.h
@@ -0,0 +1,14 @@
+#ifndef FONCTIONS_CHOIX_USER
+#define FONCTIONS_CHOIX_USER
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+void choix_invite_bibliotheque(int user_groupe);
+void choix_inscrit_bibliotheque(int user_groupe);
+void choix_admin_site_bibliotheque(int user_groupe);
+void choix_admin_general_bibliotheque(int user_groupe);
+
+#endif
--- src/choix_users/prototype_invite.h
+++
@@ -1,7 +0,0 @@
-#ifndef PROTOTYPE_INVITE_H
-#define PROTOTYPE_INVITE_H
-
-// Prototype de Liste_livres_et_dispo
-void Liste_livres_et_dispo();
-
-#endif // PROTOTYPE_INVITE_H
