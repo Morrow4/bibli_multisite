d370fbc8156a1a204ee121f7a0108f01d66f0815
malos <malosurfer02@gmail.com>
2024-01-20 18:38:20 +0100

    update switch case

--- src/choix_users/choix_inscrit.c
+++ src/choix_users/choix_inscrit.c
@@ -6,34 +6,40 @@
 // Prototypes des fonctions (à définir dans un autre fichier)
 #include "prototype_inscrit.h"
 
-void choix_admin_bibliotheque(int user_groupe) {
+void choix_admin_bibliotheque(int user_groupe)
+{
+    int choix_user;
+
     printf("+-------------Bienvenu!!-------------+\n\n");
     printf("+---------Que souhaitez vous faire?----------+\n");
-    switch(user_groupe) {
-        case 1:
-            printf("1) Lister les livres et leur disponibilité dans les différents sites\n");
-            Liste_livres_et_dispo();
-            break;
+    printf("1) Lister les livres et leur disponibilité dans les différents sites\n");
+    printf("2) Réserver un livre\n");
+    printf("3) S'inscrire sur une liste attente pour réserver un livre\n");
+    printf("4) Consulter le délai d attente pour emprunter un livre indisponible\n");
+    printf("5) Déconnexion\n");
+    scanf("%d", choix_user);
 
-        case 2:
-            printf("2) Réserver un livre\n");
-            MYSQL* conn;
-            Emprunt_soimeme(conn);
-            break;
+    switch (user_groupe)
+    {
+    case 1:
+        Liste_livres_et_dispo();
+        break;
 
-        case 3:
-            printf("3) S'inscrire sur une liste attente pour réserver un livre\n");
-            Inscri_liste_attente_livre();
-            break;
+    case 2:
+        MYSQL *conn;
+        Emprunt_soimeme(conn);
+        break;
 
-        case 4:
-            printf("4) Consulter le délai d attente pour emprunter un livre indisponible\n");
-            Affichage_delai_attente_livre();
-            break;
+    case 3:
+        Inscri_liste_attente_livre();
+        break;
 
-        default:
-            printf("5) Déconnexion\n");
-            // Fonction de déconnexion ou autre traitement
-            break;
+    case 4:
+        Affichage_delai_attente_livre();
+        break;
+
+    default:
+        // Fonction de déconnexion ou autre traitement
+        break;
     }
 }
