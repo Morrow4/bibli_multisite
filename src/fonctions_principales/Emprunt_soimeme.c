aadae7c4ae19f0a90070b8e9fe426ac710435034
Morrow <clement.laguiseray@gmail.com>
2024-01-21 00:04:09 +0100

    livre

--- src/fonctions_principales/Emprunt_soimeme.c
+++ src/fonctions_principales/Emprunt_soimeme.c
@@ -178,7 +178,7 @@ void Emprunt_soimeme(MYSQL *conn)
         {
             // Récupération de l'ISBN en variable "ISBN_true"
             char ISBN_true[14];
-            strcpy(ISBN_true, livres[numLivre - 1].ISBN);
+            strcpy(ISBN_true, Livre[numLivre - 1].ISBN);
 
             // Vérifier et effectuer l'emprunt
             verifierEtEffectuerEmprunt(conn, ISBN_true);
