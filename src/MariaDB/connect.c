e16aa1e9aa792731024cf15ea915cd1b85c8913f
boblipopi <jarcadias@gmail.com>
2024-01-21 23:03:25 +0100

    isolation

--- src/MariaDB/connect.c
+++ src/MariaDB/connect.c
@@ -46,15 +46,15 @@ void connect_database()
       exit(1);
    }
 
-   char q[255];
-   sprintf(q, "INSERT INTO Livre (ISBN, Titre, Auteur, Edition, Genre) VALUES ('test', 'test', 'test', 'test', 'test')");
+   //char q[255];
+   //sprintf(q, "INSERT INTO Livre (ISBN, Titre, Auteur, Edition, Genre) VALUES ('test', 'test', 'test', 'test', 'test')");
 
    // Exécuter la requête SQL
-   if (mysql_query(conn, q))
-   {
-      fprintf(stderr, "Erreur lors de l ajout du livre : %s\n", mysql_error(conn));
-      return;
-   }
+   //if (mysql_query(conn, q))
+   //{
+   //   fprintf(stderr, "Erreur lors de l ajout du livre : %s\n", mysql_error(conn));
+   //   return;
+   //}
 
-   printf("Succes de l ajout du livre (connect) !\n");
+   //printf("Succes de l ajout du livre (connect) !\n");
 }
