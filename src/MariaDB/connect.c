f9c6688ed9662929569613b3e392d3d0d7c27bee
boblipopi <jarcadias@gmail.com>
2024-01-21 18:47:13 +0100

    localhost db

--- src/MariaDB/connect.c
+++ src/MariaDB/connect.c
@@ -25,7 +25,7 @@ void connect_database()
    // Connexion à la base de données
    if (!mysql_real_connect(
            conn,                  // Connexion
-           "mariadb.10.1.144.49", // Hôte
+           "localhost", // Hôte
            db_user,               // Nom d'utilisateur
            db_user_password,      // Mot de passe utilisateur
            "bibliotech",          // Base de donnée par défaut
