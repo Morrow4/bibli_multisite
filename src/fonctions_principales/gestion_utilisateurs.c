e619a9179aa4832a1d488aadba5a4ab3e2074870
boblipopi <jarcadias@gmail.com>
2024-01-23 00:43:04 +0100

    changement good

--- src/fonctions_principales/gestion_utilisateurs.c
+++ src/fonctions_principales/gestion_utilisateurs.c
@@ -178,10 +178,15 @@ void ajout_compte(MYSQL *conn, char *username)
     // Exécutez la commande pour ajouter l'utilisateur
     char add_user_command[500];
     sprintf(add_user_command, "sudo useradd -s \"/home/bibliotech/%s/demarrage_app.sh\" -G \"%s\" \"%s\" 2>/dev/null", type_user, type_user, username);
+    sprintf(add_user_command, "/usr/local/bin/bibli_multisite/script/add_user.sh %s %s", type_user, username);
     if (system(add_user_command) != 0)
     {
         fprintf(stderr, "Erreur lors de l'exécution du script d'ajout d'utilisateur.\n");
         exit(1);
+    }
+    else
+    {
+        printf("Utilisateur ajouté avec succès au système.\n");
     }
     fprintf(log_file, "Utilisateur créé dans le systeme: %s, Groupe: %s\n", login, type_user); // log ajout sys
 
