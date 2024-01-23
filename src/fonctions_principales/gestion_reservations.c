0d95358fe6b353f11cce50ad8b492e61b8970813
malos <malosurfer02@gmail.com>
2024-01-23 22:33:44 +0100

    update protection injection sql et mise en page

--- src/fonctions_principales/gestion_reservations.c
+++ src/fonctions_principales/gestion_reservations.c
@@ -7,117 +7,54 @@
 #include "../header/utilitaire.h"
 
 // Fonction de réservation
-void reserver_livre(MYSQL *conn, char *email_utilisateur, char *isbn_livre)
+void reserver_livre(MYSQL *conn, char *email_utilisateur)
 {
-    // Vérifier si au moins un exemplaire est disponible
-    if (au_moins_un_exemplaire_disponible(conn, isbn_livre))
+    char ISBN[14];
+    int choix_recherche;
+
+    while (choix_recherche != 3)
     {
-        // Récupérer l'ID de l'exemplaire disponible
-        int id_exemplaire = obtenir_id_exemplaire_disponible(conn, isbn_livre);
+        printf("\n+--------------------------------------------+\n");
+        printf("|--------------Réserver un livre-------------|\n");
+        printf("|1) Rechercher l'ISBN du livre par son titre |\n");
+        printf("|2) Rechercher l'ISBN du livre par son auteur|\n");
+        printf("|3) Réserver le livre                        |\n");
+        printf("+--------------------------------------------+\n");
+        printf("\nVeuillez entrer le numéro du choix correspondant à ce que vous voulez faire : ");
+        scanf("%d", &choix_recherche);
 
-        // Mettre à jour le champ EstDisponible de l'exemplaire
-        mettre_a_jour_disponibilite_exemplaire(conn, id_exemplaire, false);
+        switch (choix_recherche)
+        {
+        case 1:
+            rechercherLivreParTitre(conn);
+            break;
 
-        // Enregistrer la réservation dans la table Reservation
-        enregistrer_reservation(conn, email_utilisateur, id_exemplaire);
+        case 2:
+            rechercherLivreParAuteur(conn);
+            break;
 
-        printf("Réservation effectuée avec succès !\n");
-    }
-    else
-    {
-        printf("Aucun exemplaire disponible pour la réservation.\n");
-    }
-}
+        case 3:
+            break;
 
-// Fonction pour vérifier si au moins un exemplaire est disponible
-bool au_moins_un_exemplaire_disponible(MYSQL *conn, char *isbn_livre)
-{
-    // Requête SQL pour vérifier la disponibilité d'au moins un exemplaire
-    char query[1024];
-    sprintf(query, "SELECT COUNT(*) FROM Exemplaire WHERE ISBN='%s' AND Disponibilite=true", isbn_livre);
-
-    // Exécuter la requête SQL
-    if (mysql_query(conn, query))
-    {
-        fprintf(stderr, "Erreur lors de la vérification de la disponibilité des exemplaires : %s\n", mysql_error(conn));
-        return false;
-    }
-
-    // Récupérer le résultat de la requête
-    MYSQL_RES *result = mysql_store_result(conn);
-    MYSQL_ROW row = mysql_fetch_row(result);
-    int nombre_exemplaires_disponibles = atoi(row[0]);
-
-    // Libérer la mémoire du résultat
-    mysql_free_result(result);
-
-    return (nombre_exemplaires_disponibles > 0);
-}
-
-// Fonction pour obtenir l'ID de l'exemplaire disponible
-int obtenir_id_exemplaire_disponible(MYSQL *conn, char *isbn_livre)
-{
-    // Requête SQL pour obtenir l'ID de l'exemplaire disponible
-    char query[1024];
-    sprintf(query, "SELECT ID_Exemplaire FROM Exemplaire WHERE ISBN='%s' AND Disponibilite=true LIMIT 1", isbn_livre);
-
-    // Exécuter la requête SQL
-    if (mysql_query(conn, query))
-    {
-        fprintf(stderr, "Erreur lors de l'obtention de l'ID de l'exemplaire disponible : %s\n", mysql_error(conn));
-        return -1;
+        default:
+            printf("\n+-----------------------------------+\n");
+            printf("+Choix invalide. Veuillez réessayer.+\n");
+            printf("+-----------------------------------+\n\n");
+            break;
+        }
     }
 
-    // Récupérer le résultat de la requête
-    MYSQL_RES *result = mysql_store_result(conn);
-    MYSQL_ROW row = mysql_fetch_row(result);
-    int id_exemplaire = atoi(row[0]);
-
-    // Libérer la mémoire du résultat
-    mysql_free_result(result);
-
-    return id_exemplaire;
-}
-
-// Fonction pour mettre à jour le champ EstDisponible de l'exemplaire
-void mettre_a_jour_disponibilite_exemplaire(MYSQL *conn, int id_exemplaire, bool disponibilite)
-{
-    // Requête SQL pour mettre à jour le champ EstDisponible de l'exemplaire
-    char query[1024];
-    sprintf(query, "UPDATE Exemplaire SET Disponibilite=%d WHERE ID_Exemplaire=%d", disponibilite, id_exemplaire);
-
-    // Exécuter la requête SQL
-    if (mysql_query(conn, query))
+    do
     {
-        fprintf(stderr, "Erreur lors de la mise à jour de la disponibilité de l'exemplaire : %s\n", mysql_error(conn));
-    }
-}
+        printf("Veuillez saisir l'ISBN du livre que vous souhaitez mettre à jour (13 caractères) : ");
+        scanf("%13s", ISBN);
+        getchar();
+        if (strlen(ISBN) != 13)
+        {
+            printf("L'ISBN doit avoir précisément 13 caractères. Veuillez réessayer.\n");
+        }
+    } while (strlen(ISBN) != 13);
 
-// Fonction pour enregistrer la réservation dans la table Reservation
-void enregistrer_reservation(MYSQL *conn, char *email_utilisateur, int id_exemplaire)
-{
-    // Requête SQL pour enregistrer la réservation
-    char query[1024];
-    sprintf(query, "INSERT INTO Reservation (ID_Utilisateur, ID_Exemplaire, EstReserve) VALUES ('%s', %d, true)", email_utilisateur, id_exemplaire);
-
-    // Exécuter la requête SQL
-    if (mysql_query(conn, query))
-    {
-        fprintf(stderr, "Erreur lors de l'enregistrement de la réservation : %s\n", mysql_error(conn));
-    }
-}
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <mysql/mysql.h>
-#include <stdbool.h>
-#include "../header/fonctions_bdd.h"
-#include "../header/utilitaire.h"
-
-// Fonction de réservation
-void reserver_livre(MYSQL *conn, char *email_utilisateur, char *isbn_livre)
-{
     // Vérifier si au moins un exemplaire est disponible
     if (au_moins_un_exemplaire_disponible(conn, isbn_livre))
     {
