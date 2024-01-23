e03d22fa449909aa3e513a8ed8d9f605fb5a7231
Morrow <clement.laguiseray@gmail.com>
2024-01-24 00:35:44 +0100

    c

--- src/fonctions_principales/Consultation_Stat.c
+++ src/fonctions_principales/Consultation_Stat.c
@@ -150,7 +150,7 @@ void consultation_stat(MYSQL *conn, int user_type) {
             printf("Veuillez choisir de consulter les statistiques pour les 3 sites ou par site : 1 | 2 : \n");
             int choix;
             scanf("%d", &choix);
-            switch (choix) 
+            switch (&choix) 
             {
                 case 1: // consultation stat pour les 3 sites
                     consultation_stat_3site(conn);
