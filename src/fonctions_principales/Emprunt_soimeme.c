46bd0224324dab36a907b968df96c5cf5f98d5ed
boblipopi <jarcadias@gmail.com>
2024-01-23 21:10:18 +0100

    test

--- src/fonctions_principales/Emprunt_soimeme.c
+++ src/fonctions_principales/Emprunt_soimeme.c
@@ -94,7 +94,7 @@ void effectuerEmprunt(MYSQL *conn, const char *ISBN, const char *username)
     // récupération de l'ID_Utilisateur = Email
     qui(username);
     printf("recuperation de l'id user : %s\n", username);
-    printf("recuperation de l'id user : %d\n", &Var_IdExemplaire);   
+    printf("recuperation de l'id user : %d\n", Var_IdExemplaire);   
     sprintf(query, "INSERT INTO Emprunt (ID_Exemplaire, ID_Utilisateur) VALUES ('%d', '%s')", &Var_IdExemplaire, username);
     if (mysql_query(conn, query) != 0)
     {
