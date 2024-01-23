f5dd74b103efc13a5cef6bcbecae18c91b3e96c6
Morrow <clement.laguiseray@gmail.com>
2024-01-23 23:45:38 +0100

    scanf inutile

--- src/fonctions_principales/Consultation_Stat.c
+++ src/fonctions_principales/Consultation_Stat.c
@@ -17,7 +17,6 @@ void consultation_stat_site(MYSQL *conn, int user_type) {
         case 1: // admin general
             while (strcmp(site, "Site A") != 0 && strcmp(site, "Site B") != 0 && strcmp(site, "Site C") != 0) {
                 printf("Choisissez un site (Site A, Site B, Site C) : ");
-                scanf("%s", &site);
                 fgets(site, sizeof(site), stdin);
                 site[strcspn(site, "\n")] = '\0'; // Supprimer le caractère de nouvelle ligne de la saisie
             }
@@ -54,10 +53,9 @@ void consultation_stat_site(MYSQL *conn, int user_type) {
     }
 
     // Saisie utilisateur pour choisir l'unité de temps
-    char unite[10];
+    char unite[20];
     while (strcmp(unite, "jour") != 0 && strcmp(unite, "mois") != 0 && strcmp(unite, "année") != 0) {
         printf("Choisissez l'unité de temps (jour, mois, année) : ");
-        scanf("%s", &unite);
         fgets(unite, sizeof(unite), stdin);
         unite[strcspn(unite, "\n")] = '\0'; // Supprimer le caractère de nouvelle ligne de la saisie
     }
@@ -102,10 +100,9 @@ void consultation_stat_site(MYSQL *conn, int user_type) {
    
 void consultation_stat_3site(MYSQL* conn) {
     // Saisie utilisateur pour choisir l'unité de temps
-    char unite[10];
+    char unite[20];
     while (strcmp(unite, "jour") != 0 && strcmp(unite, "mois") != 0 && strcmp(unite, "année") != 0) {
         printf("Choisissez l'unité de temps (jour, mois, année) : ");
-        scanf("%s", &unite);
         fgets(unite, sizeof(unite), stdin);
         unite[strcspn(unite, "\n")] = '\0'; // Supprimer le caractère de nouvelle ligne de la saisie
     }
